0afb444ba9c16bb5eb1356b32c20ebe0c277b039
Lucas <l.deutschmann@rubika-edu.com>
2019-12-17 17:16:10 +0100

    filtre

---
+++ Assets/LUD_Scripts/BatteryFill.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BatteryFill : MonoBehaviour
+{
+    public Image Fill;
+    void Start()
+    {
+        StartCoroutine(EmptyBattery());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator EmptyBattery()
+    {
+        if (Fill.fillAmount>0)
+        {
+            Fill.fillAmount -= 0.05f;
+            yield return new WaitForSeconds(3);
+            StartCoroutine(EmptyBattery());
+        }
+    }
+}
