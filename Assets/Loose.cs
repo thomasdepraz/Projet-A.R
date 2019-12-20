68d2b5fc2ac1d4ed5282a137128dd52e32163df2
Lucas <l.deutschmann@rubika-edu.com>
2019-12-20 12:11:48 +0100

    mort

---
+++ Assets/Loose.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loose : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+
+    // Update is called once per frame
+   
+ 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Ennemy")
+        {
+            SceneManager.LoadScene("Lose");
+        }
+    }
+}
